b1392da35362a0a48158b34dc87459ccbc971278
yseo14 <lmslys14@naver.com>
2025-06-13 21:45:06 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/example/stockmate/StockmateApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stockmate/StockmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
