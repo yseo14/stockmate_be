60e513020726b67dc28657168d2e800305661865
yseo14 <lmslys14@naver.com>
2025-06-17 00:28:11 +0900

    feat: 자체 로그인을 위한 passsword 필드 추가

--- src/main/java/com/example/stockmate/member/domain/Member.java
+++ src/main/java/com/example/stockmate/member/domain/Member.java
@@ -17,7 +17,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"provider", "providerId"})})
+@Table( uniqueConstraints = {@UniqueConstraint(columnNames = {"provider", "providerId"})})
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -28,7 +28,7 @@ public class Member extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     private String name;
@@ -36,14 +36,21 @@ public class Member extends BaseEntity {
     private String profileImage;
 
     @Column(nullable = false)
-    private String provider; // kakao, google
+    private String provider; // kakao, local 등
 
     @Column(nullable = false)
-    private String providerId;
+    private String providerId; // kakaoId or local일 경우 email
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Role role; // USER, ADMIN
+
+    @Column(length = 100)
+    private String password;
+
+    public boolean isSocialMember() {
+        return !this.provider.equals("local");
+    }
 
     public enum Role {
         USER, ADMIN
